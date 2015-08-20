baa39ca02d0fb95c10198cf0cf200aea8fc73684
kasicass <kasicass@gmail.com>
2015-08-20 11:28:51 +0800

    DiffuseDemo done

---
+++ src/chap10/DiffuseDemo/DiffuseDemo.cpp
@@ -0,0 +1,217 @@
+#include "d3dApp.h"
+#include "directInput.h"
+#include "gfxStats.h"
+#include "Vertex.h"
+#include <string.h>
+
+class DiffuseDemo : public D3DApp
+{
+public:
+	DiffuseDemo(HINSTANCE hInstance, std::wstring winCaption);
+	virtual ~DiffuseDemo();
+
+	virtual bool checkDeviceCaps() override;
+	virtual void onLostDevice() override;
+	virtual void onResetDevice() override;
+	virtual void updateScene(float dt) override;
+	virtual void drawScene() override;
+
+private:
+	void buildFX();
+	void buildProjMtx();
+	void buildViewMtx();
+
+private:
+	GfxStats *mGfxStats;
+
+	ID3DXMesh   *mTeapot;
+	ID3DXEffect *mFX;
+	D3DXHANDLE   mhTech;
+	D3DXHANDLE   mhWVP;
+	D3DXHANDLE   mhWorldInverseTranspose;
+	D3DXHANDLE   mhLightVecW;
+	D3DXHANDLE   mhDiffuseMtrl;
+	D3DXHANDLE   mhDiffuseLight;
+
+	D3DXVECTOR3  mLightVecW;
+	D3DXCOLOR    mDiffuseMtrl;
+	D3DXCOLOR    mDiffuseLight;
+
+	float mCameraRotationY;
+	float mCameraRadius;
+	float mCameraHeight;
+
+	D3DXMATRIX mWorld;
+	D3DXMATRIX mView;
+	D3DXMATRIX mProj;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+#if defined (DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	DiffuseDemo app(hInstance, L"Diffuse Demo");
+	gd3dApp = &app;
+
+	DirectInput dinput;
+	gDInput = &dinput;
+
+	if (gd3dApp->checkDeviceCaps())
+		return gd3dApp->run();
+	else
+		return 0;
+}
+
+DiffuseDemo::DiffuseDemo(HINSTANCE hInstance, std::wstring winCaption)
+	: D3DApp(hInstance, winCaption)
+{
+	mGfxStats = new GfxStats();
+
+	mCameraRadius    = 10.0f;
+	mCameraRotationY = 1.2 * D3DX_PI;
+	mCameraHeight    = 5.0f;
+
+	mLightVecW    = D3DXVECTOR3(0.0f, 0.0f, -1.0f);
+	mDiffuseMtrl  = D3DXCOLOR(0.0f, 1.0f, 0.0f, 1.0f);
+	mDiffuseLight = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+	D3DXMatrixIdentity(&mWorld);
+
+	HR(D3DXCreateTeapot(gd3dDevice, &mTeapot, 0));
+	buildFX();
+	
+	onResetDevice();
+
+	InitAllVertexDeclarations();
+}
+
+DiffuseDemo::~DiffuseDemo()
+{
+	SafeDelete(mGfxStats);
+
+	SafeRelease(mTeapot);
+	SafeRelease(mFX);
+
+	DestroyAllVertexDeclaration();
+}
+
+bool DiffuseDemo::checkDeviceCaps()
+{
+	D3DCAPS9 caps;
+	HR(gd3dDevice->GetDeviceCaps(&caps));
+
+	if (caps.VertexShaderVersion < D3DVS_VERSION(2,0))
+		return false;
+
+	if (caps.PixelShaderVersion < D3DPS_VERSION(2,0))
+		return false;
+
+	return true;
+}
+
+void DiffuseDemo::onLostDevice()
+{
+	mGfxStats->onLostDevice();
+	HR(mFX->OnLostDevice());
+}
+
+void DiffuseDemo::onResetDevice()
+{
+	mGfxStats->onResetDevice();
+	HR(mFX->OnResetDevice());
+
+	buildProjMtx();
+}
+
+void DiffuseDemo::updateScene(float dt)
+{
+	mGfxStats->setVertexCount(mTeapot->GetNumVertices());
+	mGfxStats->setTriCount(mTeapot->GetNumFaces());
+	mGfxStats->update(dt);
+
+	gDInput->poll();
+
+	if (gDInput->keyDown(DIK_W))
+		mCameraHeight += 25.0f * dt;
+	if (gDInput->keyDown(DIK_S))
+		mCameraHeight -= 25.0f * dt;
+
+	// divide by 50 to make mouse less sensitive
+	mCameraRotationY += gDInput->mouseDX() / 50.0f;
+	mCameraRadius    += gDInput->mouseDY() / 50.0f;
+
+	if (fabsf(mCameraRotationY) >= 2.0f * D3DX_PI)
+		mCameraRotationY = 0.0f;
+
+	if (mCameraRadius < 5.0f)
+		mCameraRadius = 5.0f;
+
+	buildViewMtx();
+}
+
+void DiffuseDemo::drawScene()
+{
+	HR(gd3dDevice->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(255,255,255), 1.0f, 0));
+	HR(gd3dDevice->BeginScene());
+
+	HR(mFX->SetTechnique(mhTech));
+
+	HR(mFX->SetMatrix(mhWVP, &(mWorld*mView*mProj)));
+	D3DXMATRIX worldInverseTranspose;
+	D3DXMatrixInverse(&worldInverseTranspose, 0, &mWorld);
+	D3DXMatrixTranspose(&worldInverseTranspose, &worldInverseTranspose);
+	HR(mFX->SetMatrix(mhWorldInverseTranspose, &worldInverseTranspose));
+	HR(mFX->SetValue(mhLightVecW, &mLightVecW, sizeof(D3DXVECTOR3)));
+	HR(mFX->SetValue(mhDiffuseMtrl, &mDiffuseMtrl, sizeof(D3DXCOLOR)));
+	HR(mFX->SetValue(mhDiffuseLight, &mDiffuseLight, sizeof(D3DXCOLOR)));
+
+	UINT numPasses = 0;
+	HR(mFX->Begin(&numPasses, 0));
+	for (UINT i = 0; i < numPasses; ++i)
+	{
+		HR(mFX->BeginPass(i));
+		HR(mTeapot->DrawSubset(0));
+		HR(mFX->EndPass());
+	}
+	HR(mFX->End());
+
+	mGfxStats->display(D3DCOLOR_XRGB(0,0,0));
+	HR(gd3dDevice->EndScene());
+	HR(gd3dDevice->Present(0, 0, 0, 0));
+}
+
+void DiffuseDemo::buildFX()
+{
+	ID3DXBuffer *errors = 0;
+	HR(D3DXCreateEffectFromFile(gd3dDevice, L"../src/chap10/DiffuseDemo/diffuse.fx",
+		0, 0, D3DXSHADER_DEBUG, 0, &mFX, &errors));
+	if (errors)
+		MessageBoxA(0, (char*)errors->GetBufferPointer(), 0, 0);
+
+	mhTech                  = mFX->GetTechniqueByName("DiffuseTech");
+	mhWVP                   = mFX->GetParameterByName(0, "gWVP");
+	mhWorldInverseTranspose = mFX->GetParameterByName(0, "gWorldInverseTranspose");
+	mhLightVecW             = mFX->GetParameterByName(0, "gLightVecW");
+	mhDiffuseMtrl           = mFX->GetParameterByName(0, "gDiffuseMtrl");
+	mhDiffuseLight          = mFX->GetParameterByName(0, "gDiffuseLight");
+}
+
+void DiffuseDemo::buildViewMtx()
+{
+	float x = mCameraRadius * cosf(mCameraRotationY);
+	float z = mCameraRadius * sinf(mCameraRotationY);
+	D3DXVECTOR3 pos(x, mCameraHeight, z);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXMatrixLookAtLH(&mView, &pos, &target, &up);
+}
+
+void DiffuseDemo::buildProjMtx()
+{
+	float w = (float)md3dPP.BackBufferWidth;
+	float h = (float)md3dPP.BackBufferHeight;
+	D3DXMatrixPerspectiveFovLH(&mProj, D3DX_PI * 0.25f, w/h, 1.0f, 5000.0f);
+}
+
---
+++ src/chap10/DiffuseDemo/diffuse.fx
@@ -0,0 +1,46 @@
+uniform extern float4x4 gWorldInverseTranspose;
+uniform extern float4x4 gWVP;
+
+uniform extern float4 gDiffuseMtrl;
+uniform extern float4 gDiffuseLight;
+uniform extern float3 gLightVecW;
+
+struct OutputVS
+{
+	float4 posH  : POSITION0;
+	float4 color : COLOR0;
+};
+
+OutputVS DiffuseVS(float3 posL : POSITION0, float3 normalL : NORMAL0)
+{
+	OutputVS outVS = (OutputVS)0;
+
+	// Transform normal to world space
+	float3 normalW = mul(float4(normalL, 0.0f), gWorldInverseTranspose).xyz;
+	normalW = normalize(normalW);
+
+	// Compute diffuse color
+	float s = max(dot(gLightVecW, normalW), 0.0f);
+	outVS.color.rgb = s*(gDiffuseMtrl*gDiffuseLight).rgb;
+	outVS.color.a   = gDiffuseMtrl.a;
+
+	// Transform to homogeneous clip space
+	outVS.posH = mul(float4(posL, 1.0f), gWVP);
+
+	return outVS;
+}
+
+float4 DiffusePS(float4 c : COLOR0) : COLOR
+{
+	return c;
+}
+
+technique DiffuseTech
+{
+	pass P0
+	{
+		vertexShader = compile vs_2_0 DiffuseVS();
+		pixelShader  = compile ps_2_0 DiffusePS();
+	}
+}
+
