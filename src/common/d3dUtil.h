e37dde36a54b3c7fa85c99a21e119069adfd1847
kasicass <kasicass@gmail.com>
2016-05-06 18:20:40 +0800

    12_TeapotDemo, alpha blending

--- src/common/d3dUtil.h
+++ src/common/d3dUtil.h
@@ -43,6 +43,29 @@ extern IDirect3DDevice9* gd3dDevice;
 	#endif
 #endif
 
+//===============================================================
+// Geometry generation.
 
 void GenTriGrid(int numVertRows, int numVertCols, float dx, float dz, 
 				const D3DXVECTOR3 &center, std::vector<D3DXVECTOR3> &verts, std::vector<DWORD> &indices);
+
+//===============================================================
+// Colors and Materials
+
+const D3DXCOLOR WHITE(1.0f, 1.0f, 1.0f, 1.0f);
+const D3DXCOLOR BLACK(0.0f, 0.0f, 0.0f, 1.0f);
+const D3DXCOLOR RED(1.0f, 0.0f, 0.0f, 1.0f);
+const D3DXCOLOR GREEN(0.0f, 1.0f, 0.0f, 1.0f);
+const D3DXCOLOR BLUE(0.0f, 0.0f, 1.0f, 1.0f);
+
+struct Material
+{
+	Material() : ambient(WHITE), diffuse(WHITE), spec(WHITE), specPower(8.0f) {}
+	Material(const D3DXCOLOR& a, const D3DXCOLOR& d, const D3DXCOLOR& s, float power) :
+		ambient(a), diffuse(d), spec(s), specPower(power) {}
+	
+	D3DXCOLOR ambient;
+	D3DXCOLOR diffuse;
+	D3DXCOLOR spec;
+	float specPower;
+};
